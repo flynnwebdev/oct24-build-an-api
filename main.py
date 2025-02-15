0cbbced7f643a5c55f2fbd86fb451e924d4d5bee
Mr. Robot <flynnwebdev@gmail.com>
2025-02-15 10:38:07 +1000

    Setup dependencies and database

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from init import db, ma
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    return app
